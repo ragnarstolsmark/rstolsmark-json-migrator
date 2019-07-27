bdc05e7dc6207b95ceb4c8bd5199905ef05fab67
Ragnar Stølsmark <rstolsmark@live.no>
2019-07-27 13:26:53 +0200

    Simplified migrator

--- src/__tests__/Migrate.test.ts
+++ src/__tests__/Migrate.test.ts
@@ -1,113 +1,63 @@
-import { IMigration, Migrate } from "../index";
+import Migrate, { IMigration, IAppliedMigration } from "../index";
 
 let objectToMigrate: any;
 
 function getMigrations(): IMigration[] {
-  return [
+  const twoFirst = getTwoFirstMigrations();
+  return twoFirst.concat([
     {
-      name: "Delta01 - Add name",
-      up: () => {
-        objectToMigrate.name = "";
-      },
-      down: () => {
-        delete objectToMigrate.name;
-      }
-    },
-    {
-      name: "Delta02 - Add telephone",
-      up: () => {
-        objectToMigrate.telephone = "";
-      },
-      down: () => {
-        delete objectToMigrate.telephone;
-      }
-    },
-    {
-      name: "Delta03 - Add age",
+      name: "Delta03",
       up: () => {
         objectToMigrate.age = 0;
-      },
-      down: () => {
-        delete objectToMigrate.age;
       }
     },
     {
-      name: "Delta04 - Add address",
+      name: "Delta04",
       up: () => {
         objectToMigrate.address = "";
-      },
-      down: () => {
-        delete objectToMigrate.address;
+      }
+    }
+  ]);
+}
+
+function getTwoFirstMigrations() {
+  return [
+    {
+      name: "Delta01",
+      up: () => {
+        objectToMigrate.name = "";
+      }
+    },
+    {
+      name: "Delta02",
+      up: () => {
+        objectToMigrate.telephone = "";
       }
     }
   ];
 }
 
-test("Run Initial migration", () => {
+test("Run migrations", () => {
   objectToMigrate = {};
   const migrations = getMigrations();
-  Migrate(migrations, undefined, "Delta01 - Add name");
-  expect(objectToMigrate.name).toBeDefined();
-});
-
-test("Reverse Initial migration", () => {
-  objectToMigrate = {
-    name: ""
-  };
-  const migrations = getMigrations();
-  Migrate(migrations, "Delta01 - Add name", "Non-existing migration");
-  expect(objectToMigrate.name).toBeUndefined();
-});
-
-test("Migrate to latest version from nothing", () => {
-  objectToMigrate = {};
-  const migrations = getMigrations();
-  Migrate(migrations);
+  Migrate(migrations, []);
   expect(objectToMigrate.name).toBeDefined();
   expect(objectToMigrate.telephone).toBeDefined();
   expect(objectToMigrate.age).toBeDefined();
   expect(objectToMigrate.address).toBeDefined();
 });
 
-test("Migrate from delta 04 to delta 03", () => {
-  objectToMigrate = {
-    name: "",
-    telephone: "",
-    age: 0,
-    address: ""
-  };
-  const migrations = getMigrations();
-  Migrate(migrations, "Delta04 - Add address", "Delta03 - Add age");
-  expect(objectToMigrate.name).toBeDefined();
-  expect(objectToMigrate.telephone).toBeDefined();
-  expect(objectToMigrate.age).toBeDefined();
-  expect(objectToMigrate.address).toBeUndefined();
-});
-
-test("Migrate from delta 02 to delta 04", () => {
-  objectToMigrate = {
-    name: "",
-    telephone: ""
-  };
-  const migrations = getMigrations();
-  Migrate(migrations, "Delta02 - Add age", "Delta04 - Add address");
-  expect(objectToMigrate.name).toBeDefined();
-  expect(objectToMigrate.telephone).toBeDefined();
-  expect(objectToMigrate.age).toBeDefined();
-  expect(objectToMigrate.address).toBeDefined();
-});
-
-test("Migrate from delta 04 to delta 02", () => {
-  objectToMigrate = {
-    name: "",
-    telephone: "",
-    age: 0,
-    address: ""
-  };
-  const migrations = getMigrations();
-  Migrate(migrations, "Delta04 - Add address", "Delta02 - Add telephone");
+test("Migrate from nothing to delta 04 via delta 02", () => {
+  objectToMigrate = {};
+  const alreadyRunMigrations = [] as IAppliedMigration[];
+  Migrate(getTwoFirstMigrations(), alreadyRunMigrations);
   expect(objectToMigrate.name).toBeDefined();
   expect(objectToMigrate.telephone).toBeDefined();
   expect(objectToMigrate.age).toBeUndefined();
   expect(objectToMigrate.address).toBeUndefined();
+  Migrate(getMigrations(), alreadyRunMigrations);
+  expect(objectToMigrate.name).toBeDefined();
+  expect(objectToMigrate.telephone).toBeDefined();
+  expect(objectToMigrate.age).toBeDefined();
+  expect(objectToMigrate.address).toBeDefined();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,27 @@
 export interface IMigration {
   name: string;
   up: () => void;
-  down?: () => void;
+}
+
+export interface IAppliedMigration {
+  name: string;
+  dateApplied: Date;
 }
 
 export class MigrationError extends Error {}
 
-export function Migrate(
+export default function Migrate(
   migrations: IMigration[],
-  currentVersion?: string,
-  versionToMigrateTo?: string
+  appliedMigrations: IAppliedMigration[]
 ) {
-  if (!migrations.length) {
-    // no migrations, just return;
-    return;
-  }
-  if (!versionToMigrateTo) {
-    versionToMigrateTo = migrations[migrations.length - 1].name;
-  }
-  let fromIndex = -1; // initial version
-  let toIndex = -1;
-  migrations.forEach((migration, index) => {
-    if (migration.name === currentVersion) {
-      fromIndex = index;
-    }
-    if (migration.name === versionToMigrateTo) {
-      toIndex = index;
-    }
+  const migrationsToRun = migrations.filter(
+    m => appliedMigrations.filter(a => a.name === m.name).length === 0
+  );
+  migrationsToRun.forEach(m => {
+    m.up();
+    appliedMigrations.push({
+      name: m.name,
+      dateApplied: new Date()
+    });
   });
-  if (fromIndex === toIndex) {
-    // Already migrated, just return
-    return;
-  }
-  const migrateUp = fromIndex < toIndex;
-  if (migrateUp) {
-    for (let i = fromIndex + 1; i <= toIndex; i++) {
-      migrations[i].up();
-    }
-  } else {
-    for (let i = fromIndex; i > toIndex; i--) {
-      const migration = migrations[i];
-      if (!migration.down) {
-        throw new MigrationError(
-          `No down migration defined for migration ${migration.name}`
-        );
-      }
-      migration.down();
-    }
-  }
 }
